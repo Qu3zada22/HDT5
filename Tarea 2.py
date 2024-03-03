074596ad88db0e5f96753e2d12dbb4b9d3e5d099
Qu3zada22 <139565119+Qu3zada22@users.noreply.github.com>
2024-03-03 15:29:54 -0600

    PARTE 2, tarea dos

--- Tarea 2.py
+++ Tarea 2.py
@@ -4,10 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 RANDOM_SEED = 42
-NUM_PROCESSES = 25
 CPU_SPEED = 3  # Velocidad del CPU (instrucciones por unidad de tiempo)
 MEM_CAPACITY = 100  # Capacidad de la memoria RAM
-INTERVAL = 10  # Intervalo para la distribución exponencial de llegada de procesos
 
 waiting_times = []  # Lista para almacenar los tiempos de espera de cada proceso
 ram_utilization = []  # Lista para almacenar la utilización de RAM en cada instante
@@ -48,33 +46,47 @@ class Process:
             yield self.env.process(cpu.execute(self))
             ram_utilization.append(MEM_CAPACITY - ram.level)
 
-
-def setup(env, num_processes, cpu, ram):
+def setup(env, num_processes, cpu, ram, interval):  # Añadir intervalo como parámetro
     for i in range(num_processes):
         ram_request = random.randint(1, 10)  # Solicita una cantidad aleatoria de RAM entre 1 y 10
         p = Process(env, i, ram_request)
         env.process(p.run(cpu, ram))
-        t = random.expovariate(1.0 / INTERVAL)
+        t = random.expovariate(1.0 / interval)  # Utilizar interval como se pasa al método
         yield env.timeout(t)
 
+def run_simulation(num_processes, interval):
+    env = simpy.Environment()
+    cpu = CPU(env)
+    ram = simpy.Container(env, capacity=MEM_CAPACITY, init=MEM_CAPACITY)  # Inicializa la RAM con su capacidad máxima
+    env.process(setup(env, num_processes, cpu, ram, interval))  # Pasar interval como parámetro
+    env.run()
 
-env = simpy.Environment()
-cpu = CPU(env)
-ram = simpy.Container(env, capacity=MEM_CAPACITY, init=MEM_CAPACITY)  # Inicializa la RAM con su capacidad máxima
-env.process(setup(env, NUM_PROCESSES, cpu, ram))
-env.run()
+    # Cálculo de promedio de tiempo de espera
+    if len(waiting_times) > 0:
+        avg_time = np.mean(waiting_times)
+        print("Promedio de tiempo de espera:", avg_time)
+    else:
+        print("No se han completado suficientes procesos para calcular el promedio de tiempo de espera.")
 
-# Cálculo de promedio de tiempo de espera
-if len(waiting_times) > 0:
-    avg_time = np.mean(waiting_times)
-    print("Promedio de tiempo de espera:", avg_time)
-else:
-    print("No se han completado suficientes procesos para calcular el promedio de tiempo de espera.")
+    # Gráfica de utilización de RAM
+    plt.plot(range(len(ram_utilization)), ram_utilization)
+    plt.xlabel("Tiempo")
+    plt.ylabel("Utilización de RAM")
+    plt.title("Utilización de RAM durante la simulación")
+    plt.grid(True)
+    plt.show()
 
-# Gráfica de utilización de RAM
-plt.plot(range(len(ram_utilization)), ram_utilization)
-plt.xlabel("Tiempo")
-plt.ylabel("Utilización de RAM")
-plt.title("Utilización de RAM durante la simulación")
-plt.grid(True)
-plt.show()
+# Llamadas a la función de simulación con diferentes parámetros
+print("Simulación con intervalo de 10")
+run_simulation(25, 10)
+run_simulation(50, 10)
+run_simulation(100, 10)
+run_simulation(150, 10)
+run_simulation(200, 10)
+
+print("Simulación con intervalo de 1")
+run_simulation(25, 1)
+run_simulation(50, 1)
+run_simulation(100, 1)
+run_simulation(150, 1)
+run_simulation(200, 1)
